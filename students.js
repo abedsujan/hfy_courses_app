bc8ff500e667701c92403d45bb15d46a3827bb97
Abed Sujan <abedsujan@gmail.com>
2019-05-19 23:13:06 +0200

    week01 homework

---
+++ students.js
@@ -0,0 +1,25 @@
+const students =[
+    {
+        name: 'rahim',
+        class: '09'
+    },
+    {
+        name: 'Nayana',
+        class: '09'
+    },
+    {
+        name: 'Jastina',
+        class: '09'
+    },
+    {
+        name: 'Ranim',
+        class: '09'
+    },
+    {
+        name: 'Hema',
+        class: '09'
+    },
+];
+
+
+module.exports = students;
